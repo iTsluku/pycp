97fc7b874bc443aef10658427b2de45196a63556
einwille <einwille@fim.uni-passau.de>
2022-09-25 00:25:54 +0200

    Refactor decorator for class test case

--- test/cpin_test.py
+++ test/cpin_test.py
@@ -4,52 +4,44 @@ from unittest.mock import patch
 from src import cpin
 
 
+@patch("builtins.input")
 class TestCompetitiveProgrammingInput(unittest.TestCase):
-    @patch("builtins.input")
     def test_single_int(self, mock_input):
         mock_input.return_value = "7"
         single_int = cpin.si()
         assert single_int == 7
 
-    @patch("builtins.input")
     def test_multiple_int(self, mock_input):
         mock_input.return_value = "7 3 2"
         multiple_int = cpin.mi()
         assert multiple_int == [7, 3, 2]
 
-    @patch("builtins.input")
     def test_single_str(self, mock_input):
         mock_input.return_value = "7"
         single_str = cpin.ss()
         assert single_str == "7"
 
-    @patch("builtins.input")
     def test_multiple_str(self, mock_input):
         mock_input.return_value = "7 3 2"
         multiple_str = cpin.ms()
         assert multiple_str == ["7", "3", "2"]
 
-    @patch("builtins.input")
     def test_multiple_int_with_sep(self, mock_input):
         mock_input.return_value = "7,3,2"
         multiple_int = cpin.mi(sep=",")
         assert multiple_int == [7, 3, 2]
 
-    @patch("builtins.input")
     def test_multiple_str_with_sep(self, mock_input):
         mock_input.return_value = "7.3.2"
         multiple_str = cpin.ms(sep=".")
         assert multiple_str == ["7", "3", "2"]
 
-    @patch("builtins.input")
     def test_multiple_str_with_maxsplit(self, mock_input):
         mock_input.return_value = "7 3 2"
         multiple_str = cpin.ms(maxsplit=1)
         assert multiple_str == ["7", "3 2"]
 
-    @patch("builtins.input")
     def test_multiple_int_with_maxsplit(self, mock_input):
         mock_input.return_value = "7 3 2"
         with self.assertRaises(ValueError):
-            # [7, "3 2"]
             multiple_int = cpin.mi(maxsplit=1)
